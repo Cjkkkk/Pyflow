a3a702e3fe8ec247dbb42e40b14ed71a4f91ec0b
cjkkkk <656569648@qq.com>
2019-10-31 22:51:00 +0800

    rename class

--- flow/autograd.py
+++ flow/autograd.py
@@ -1,109 +1,109 @@
 import numpy as np
 from .tensor import Tensor
 
-class Operation:
+class Function:
     def __init__(self):
         pass
     
     def __call__(self):
-        return self.f()
+        return self.backward()
     
-    def f(self):
-        raise NotImplementedError("should implement forward method")
+    def backward(self):
+        raise NotImplementedError("should implement backward method")
 
-class AddOperation(Operation):
+class AddFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.b.grad = self.output.grad * 1
         self.a.grad = self.output.grad * 1
         self.a.backward()
         self.b.backward()
 
-class MulOperation(Operation):
+class MulFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.b.grad = self.output.grad * self.a.data
         self.a.grad = self.output.grad * self.b.data
         self.a.backward()
         self.b.backward()
 
-class SubOperation(Operation):
+class SubFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.b.grad = self.output.grad * (-1)
         self.a.grad = self.output.grad * 1
         self.a.backward()
         self.b.backward()
 
-class TruedivOperation(Operation):
+class TruedivFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.b.grad = self.output.grad * (-self.a.data) / (self.b.data ** 2)
         self.a.grad = self.output.grad / self.b.data
         self.a.backward()
         self.b.backward()
 
-class PowOperation(Operation):
+class PowFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         pass
 
-class MMOperation(Operation):
+class MMFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.b.grad = np.matmul(np.transpose(self.a.data), self.output.grad)
         self.a.grad = np.matmul(self.output.grad, np.transpose(self.b.data))
         self.a.backward()
         self.b.backward()
 
-class SumOperation(Operation):
+class SumFunction(Function):
     def __init__(self, output, a):
         super().__init__()
         self.a = a
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.a.grad = np.ones(self.a.data.shape)
         self.a.backward()
 
-class SquareLossOperation(Operation):
+class SquareLossFunction(Function):
     def __init__(self, output, a, b):
         super().__init__()
         self.a = a
         self.b = b
         self.output = output
     
-    def f(self):
+    def backward(self):
         self.a.grad = 2.0 * self.a.data
         self.b.grad = -2.0 * self.b.data
         self.a.backward()
--- flow/function.py
+++ flow/function.py
@@ -3,48 +3,48 @@ from .tensor import Tensor
 import numpy as np
 
 
-def register_backward_hook(output, operation, *input):
-    output.grad_fn = operation(output, *input)
+def register_backward_hook(output, function, *input):
+    output.grad_fn = function(output, *input)
     output.is_leaf = False
     for i in input: # if all input does not require grad, output does not require grad
         output.require_grad = i.require_grad or output.require_grad
 
 def add(a, b):
     new_tensor = Tensor(a.data + b.data)
-    register_backward_hook(new_tensor, autograd.AddOperation, a, b)
+    register_backward_hook(new_tensor, autograd.AddFunction, a, b)
     return new_tensor
 
 def mul(a, b):
     new_tensor = Tensor(a.data * b.data)
-    register_backward_hook(new_tensor, autograd.MulOperation, a, b)
+    register_backward_hook(new_tensor, autograd.MulFunction, a, b)
     return new_tensor
 
 def sub(a, b):
     new_tensor = Tensor(a.data - b.data)
-    register_backward_hook(new_tensor, autograd.SubOperation, a, b)
+    register_backward_hook(new_tensor, autograd.SubFunction, a, b)
     return new_tensor
 
 def truediv(a, b):
     new_tensor = Tensor(a.data / b.data)
-    register_backward_hook(new_tensor, autograd.TruedivOperation, a, b)
+    register_backward_hook(new_tensor, autograd.TruedivFunction, a, b)
     return new_tensor
 
 def pow(a, b):
     new_tensor = Tensor(a.data ** b.data)
-    register_backward_hook(new_tensor, autograd.PowOperation, a, b)
+    register_backward_hook(new_tensor, autograd.PowFunction, a, b)
     return new_tensor
 
 def mm(a, b):
     new_tensor = Tensor(np.matmul(a.data, b.data))
-    register_backward_hook(new_tensor, autograd.MMOperation, a, b)
+    register_backward_hook(new_tensor, autograd.MMFunction, a, b)
     return new_tensor
 
 def sum_(a):
     new_tensor = Tensor(np.sum(a.data))
-    register_backward_hook(new_tensor, autograd.SumOperation, a)
+    register_backward_hook(new_tensor, autograd.SumFunction, a)
     return new_tensor
 
 def square_loss(a, b):
     new_tensor = Tensor(np.sum(np.square(a.data - b.data)))
-    register_backward_hook(new_tensor, autograd.SquareLossOperation, a, b)
+    register_backward_hook(new_tensor, autograd.SquareLossFunction, a, b)
     return new_tensor
