4204c4b775f6cab4a371d732ec08b80961c53666
Cjkkkk <656569648@qq.com>
2020-02-13 15:00:41 +0800

    Update autograd.py

--- flow/autograd.py
+++ flow/autograd.py
@@ -4,76 +4,18 @@ from .tensor import Tensor
 class Function:
     def __init__(self, *args):
         self.inputs = args
+        output = self.forward(*args)
+        self.register_backward_hook(output)
+        return output
     
-    def __call__(self):
-        return self.backward()
-    
-    def backward(self):
+    def register_backward_hook(self, output):
+        output.grad_fn = self.backward
+        output.is_leaf = False
+        for i in self.inputs: # if all input does not require grad, output does not require grad
+            output.require_grad = i.require_grad or output.require_grad
+        
+    def forward(self, *args):
+        raise NotImplementedError("should implement forward method")
+       
+    def backward(self, grad_output):
         raise NotImplementedError("should implement backward method")
-
-class AddFunction(Function):
-    def __init__(self, *args):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        b_grad = grad_output * 1
-        a_grad = grad_output * 1
-        return a_grad, b_grad
-
-class MulFunction(Function):
-    def __init__(self, *args):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        a, b = self.inputs
-        b_grad = grad_output * a.data
-        a_grad = grad_output * b.data
-        return a_grad, b_grad
-
-class SubFunction(Function):
-    def __init__(self, *args):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        b_grad = grad_output * (-1)
-        a_grad = grad_output * 1
-        return a_grad, b_grad
-
-class TruedivFunction(Function):
-    def __init__(self, *args):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        a, b = self.inputs
-        b_grad = grad_output * (-a.data) / (b.data ** 2)
-        a_grad = grad_output / b.data
-        return a_grad, b_grad
-
-class MMFunction(Function):
-    def __init__(self, *args):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        a, b = self.inputs
-        b_grad = np.matmul(np.transpose(a.data), grad_output)
-        a_grad = np.matmul(grad_output, np.transpose(b.data))
-        return a_grad, b_grad
-
-class SumFunction(Function):
-    def __init__(self, a):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        a = self.inputs[0]
-        a_grad = np.ones(a.data.shape)
-        return a_grad
-
-class SquareLossFunction(Function):
-    def __init__(self, *args):
-        super().__init__(*args)
-    
-    def backward(self, grad_output):
-        a, b = self.inputs
-        a_grad = 2.0 * a.data
-        b_grad = -2.0 * b.data
-        return a_grad, b_grad
