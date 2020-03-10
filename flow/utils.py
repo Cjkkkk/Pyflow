b806ae4f2f2b509dafb791bf3951adc9a85f087b
cjkkkk <656569648@qq.com>
2020-03-11 01:45:32 +0800

    fix compute_loc bug

--- flow/utils.py
+++ flow/utils.py
@@ -3,11 +3,10 @@ from .tensor import Tensor
 
 def compute_loc(idx, shape):
     loc = [0] * len(shape)
-    for i in range(len(shape) - 1):
-        prod = np.prod(shape[i + 1:])
+    for i in range(len(shape)):
+        prod = int(np.prod(shape[i + 1:]))
         loc[i] = idx // prod
         idx = idx % prod
-    loc[-1] = idx % shape[-1]
     return tuple(loc)
 
 def gradient_check(f, *args):
@@ -27,5 +26,4 @@ def gradient_check(f, *args):
                 out_eps = f(*args)
                 gradient[loc] = np.sum(out_eps.data - out.data) / eps
                 arg.data[loc] -= eps
-            print(gradient, arg.grad)
             assert np.allclose(gradient, arg.grad)
