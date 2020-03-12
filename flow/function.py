fix logsoftmax gradient

flow/function.py
@@ -243,17 +243,26 @@ class View(autograd.Function):
 class LogSoftmax(autograd.Function):
     @staticmethod
     def forward(ctx, tensor):
+        # tensor size is (N, C)
         data = tensor.data
-        pro_sum = np.sum(data)
-        pro = data / pro_sum
-        log_pro = np.log(pro)
-        ctx.save_for_backward(pro)
-        return Tensor(log_pro)
+        data_shift = data - np.max(data)
+        data_shift_exp = np.exp(data_shift)
+        exp_sum = np.sum(data_shift_exp, axis=1, keepdims=True)
+        res = data_shift - np.log(exp_sum)
+        ctx.save_for_backward(data_shift_exp, exp_sum)
+        return Tensor(res)
     
     @staticmethod
     def backward(ctx, grad_output):
-        pro = ctx.saved_tensors()[0]
-        return grad_output * (1 - pro)
+        data_shift_exp, exp_sum = ctx.saved_tensors()
+        e = - data_shift_exp / exp_sum
+        N, C = e.shape
+        grad = np.zeros((N, C))
+        for i in range(N):
+            jac = np.tile(e[i], (C, 1))
+            jac[np.diag_indices_from(jac)] += 1
+            grad[i] = np.matmul(np.transpose(jac), grad_output[i])
+        return grad
 
 class NllLoss(autograd.Function):
     @staticmethod

test/test_gradient.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import torch
+import torch.nn.functional as PF
 
 import flow.function as F
 from flow.tensor import Tensor
@@ -9,16 +10,31 @@ from flow.module import MaxPool2d, Conv2d
 
 
 
-class TestGradient(unittest.TestCase):
+class TestGradientAuto(unittest.TestCase):
     # check using gradient_check tools
-    def test_sum(self):
+    def test_sum_auto(self):
         gradient_check(F.sum_, Tensor([[1,2,3,4], [5, 6, 7, 8]], require_grad=True))
 
-    def test_nll_loss(self):
+    def test_nll_loss_auto(self):
         gradient_check(F.nll_loss, Tensor([[1, 2, 3, 4], [5, 6, 7, 8]], require_grad=True), Tensor(np.array([0, 1])), True)
 
+    def test_logsoftmax_auto(self):
+        gradient_check(F.log_softmax, Tensor([[1,2,3,4], [5, 6, 7, 8]], require_grad=True))
 
+class TestGradientPytorch(unittest.TestCase):
     # check using pytorch
+    def test_logsoftmax(self):
+        torch_input = torch.rand((3, 2), requires_grad=True)
+        torch_output = PF.log_softmax(torch_input, dim=1)
+        torch_output.backward(torch.ones(torch_output.shape))
+
+        flow_input = Tensor(torch_input.detach().numpy(), require_grad=True)
+        flow_output = F.log_softmax(flow_input)
+        flow_output.backward()
+        
+        assert np.allclose(torch_output.detach().numpy(), flow_output.data)
+        assert np.allclose(torch_input.grad.detach().numpy(), flow_input.grad)
+
     def test_conv2d(self):
         torch_conv2d = torch.nn.Conv2d(3, 2, 2, bias=False)
         torch_input = torch.rand((1, 3, 3, 3), requires_grad=True)